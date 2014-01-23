34e5b8b70fa75f3a1081d66250c44a7f420ded54
kubov <kubov2@gmail.com>
2014-01-23 15:33:36 +0100

    inital commit

---
+++ problemy_spoleczne.mm
@@ -0,0 +1,31 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1390222384368" ID="ID_932937079" MODIFIED="1390225754482" TEXT="PSIZI">
+<node CREATED="1390223126612" HGAP="56" ID="ID_612933193" MODIFIED="1390224102325" POSITION="right" TEXT="Prawo - system norm prawnych, zib&#xf3;r og&#xf3;nych, abstrakcyjnych i jednoznaczych dyrketyw post&#x119;powania kt&#xf3;re powsta&#x142;y w zwi&#x105;zku z instantienik i funkcjonowaniem pa&#x144;astwa lub innego uporz&#x105;dkowanego organu spo&#x142;&#x119;cznego" VSHIFT="-112">
+<node CREATED="1390223185417" HGAP="56" ID="ID_1268814785" MODIFIED="1390224108060" TEXT="Norma prawna - najmnijeszy stanowi&#x105;cy sensown&#x105;&#xa0;ca&#x142;o&#x15b;&#x107;&#xa0;element prawa regu&#x142;&#x105; post&#x119;powania zewn&#x119;tzrznego stworzona na posdstwie przepisow prawnych&#xa;(ZDEFINIOWANE I USTANOWIONE)" VSHIFT="87"/>
+<node CREATED="1390223366951" HGAP="27" ID="ID_804834788" MODIFIED="1390224104526" TEXT="Moralno&#x15b;&#x107;&#xa0;ukszta&#x142;towany w procesie d&#x142;ugotrwa&#x142;ego rozwoju spo&#x142;eczeostwa. Zesp&#xf3;&#x142; norm post&#x119;powania wg. kt&#xf3;rych ocenia si&#x119; okre&#x15b;lenia takie jak DOBRE lub Z&#x141;E.&#xa;ISTNIEJ&#x104; W SWIADOMOSCI SPOLECZENSTWA" VSHIFT="66"/>
+</node>
+<node CREATED="1390223950767" HGAP="82" ID="ID_1715769770" MODIFIED="1390225148529" POSITION="left" TEXT="Umowy" VSHIFT="-145">
+<node CREATED="1390223953050" ID="ID_897466123" MODIFIED="1390223955407" TEXT="o dzie&#x142;o">
+<node CREATED="1390223964336" HGAP="37" ID="ID_611107930" MODIFIED="1390224064640" TEXT="dotycz&#x105; wykonania okreslonego dzie&#x142;a, to ono jest przedmiotem umowy" VSHIFT="-43">
+<node CREATED="1390223980666" ID="ID_1792217582" MODIFIED="1390223984251" TEXT="materialne"/>
+<node CREATED="1390223985853" ID="ID_990806221" MODIFIED="1390223990876" TEXT="nimaterialne"/>
+</node>
+<node CREATED="1390223994863" HGAP="84" ID="ID_1290322240" MODIFIED="1390224093990" TEXT="dotycz&#x105; faktu wykonywania pracy, nizaleznie od rezultat&#xf3;w, ale jest to umowa starannego dzia&#x142;ania" VSHIFT="90"/>
+</node>
+<node CREATED="1390223956171" ID="ID_901144205" MODIFIED="1390223963076" TEXT="zlecenia"/>
+</node>
+<node CREATED="1390224957272" HGAP="151" ID="ID_67316433" MODIFIED="1390226243465" POSITION="right" TEXT="Prawo autorksie" VSHIFT="-100">
+<node CREATED="1390226148648" ID="ID_844272373" MODIFIED="1390226149594" TEXT="Utw&#xf3;r ka&#x17c;dy przejaw dzia&#x142;alno&#x15b;ci tw&#xf3;rczej o indywidualnym charakterze, ustalony w jakiejkolwiek postaci, niezale&#x17c;nie od warto&#x15b;ci, przeznaczenia i sposobu wyra&#x17c;enia."/>
+<node CREATED="1390226216187" ID="ID_1723272928" MODIFIED="1390226221890" TEXT="autorskie prawa maj&#x105;tkowe"/>
+<node CREATED="1390226243465" ID="ID_1766966590" MODIFIED="1390226247122" TEXT="autorskie prawa osobiste4"/>
+</node>
+<node CREATED="1390225129927" HGAP="180" ID="ID_1044175879" MODIFIED="1390225681457" POSITION="left" TEXT="Ochrona danych osobowych" VSHIFT="34">
+<node CREATED="1390225660557" ID="ID_1204750189" MODIFIED="1390225717309" TEXT="Dane osobowe">
+<node CREATED="1390225717833" HGAP="28" ID="ID_1952949004" MODIFIED="1390225726499" TEXT="za dane osobowe uwa&#x17c;a si&#x119; wszelkie informacje dotycz&#x105;ce zidentyfikowanej lub mo&#x17c;liwej do zidentyfikowania osoby fizycznej. Osob&#x105; mo&#x17c;liw&#x105; do zidentyfikowania jest osoba, kt&#xf3;rej to&#x17c;samo&#x15b;&#x107; mo&#x17c;na okre&#x15b;li&#x107; bezpo&#x15b;rednio lub po&#x15b;rednio, w szczeg&#xf3;lno&#x15b;ci przez powo&#x142;anie si&#x119; na numer identyfikacyjny albo jeden lub kilka specyficznych czynnik&#xf3;w okre&#x15b;laj&#x105;cych jej cechy fizyczne, fizjologiczne, umys&#x142;owe, ekonomiczne, kulturowe lub spo&#x142;eczne. Informacji nie uwa&#x17c;a si&#x119; za umo&#x17c;liwiaj&#x105;c&#x105; okre&#x15b;lenie to&#x17c;samo&#x15b;ci osoby, je&#x17c;eli wymaga&#x142;oby to nadmiernych koszt&#xf3;w, czasu lub dzia&#x142;a&#x144;." VSHIFT="-62"/>
+</node>
+<node CREATED="1390225681457" ID="ID_1767747528" MODIFIED="1390225683270" TEXT="regulacje prawne dotycz&#x105;ce tworzenia i pos&#x142;ugiwania si&#x119; zbiorami danych osobowych, a tak&#x17c;e pojedynczymi danymi, maj&#x105;ce na celu administracyjno-prawn&#x105; ochron&#x119; prawa do prywatno&#x15b;ci, tzn. mo&#x17c;liwo&#x15b;d jednostki lub grupy os&#xf3;b do utrzymania swych osobistych zwyczaj&#xf3;w i zachowao z dala od widoku publicznego."/>
+</node>
+<node CREATED="1390225755254" ID="ID_1880760397" MODIFIED="1390225840700" POSITION="right" TEXT="Prawo spadku"/>
+</node>
+</map>
